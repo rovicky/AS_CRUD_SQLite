67ac9a5ac55ef6722e9aabfbd4816b69b4ad24a1
rovicky <rovicky3@gmail.com>
2020-10-22 13:01:19 +0700

    AS_CRUD_SQLite

---
+++ app/src/main/java/com/example/crud_sqlite/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.crud_sqlite;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    EditText editNama, editAlamat;
+    Button btnSimpan;
+    SharedPreferences sharedPreferences;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        editNama = findViewById(R.id.nama);
+    }
+}
